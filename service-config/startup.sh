8208e17dc764b4b307ac707cf8def2bf21eb5321
Piotr Szydelko <wiertel@wiertel.info>
2022-12-14 07:25:32 +0000

    customize nginx config

--- service-config/startup.sh
+++ service-config/startup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
+cp /home/site/wwwroot/service-config/nginx /etc/nginx/sites-available/default
+service reload nginx
+
 echo "hello"
 date | tee /tmp/nothing
+set > /tmp/environ
